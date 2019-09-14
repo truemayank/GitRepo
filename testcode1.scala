fec6921b064ac70f157e4755776ab095b153bceb
Mayank Gupta <seekmayank@gmail.com>
2019-09-13 22:09:10 -0700

    seek commit 2
    seek commit 2

--- testcode1.scala
+++ testcode1.scala
@@ -2,4 +2,5 @@
 val x = Array("Yes","y","Y")
 
 if ( x == "Yes" &&
-     x == "Y") //changes by seek to make it robust
+     x == "Y" && //changes by seek to make it robust
+     x == 'y' ) //changes by seek based on review
